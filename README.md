84456b56f77428a955ea61fe8f63445047874bd7
iwamizawa-software <iwamizawasoftware@icloud.com>
2020-06-16 19:36:16 +0900

    beta

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# url-shortener
-url-shortener web extension
+# URL Shortener
+License is [![CC0](https://licensebuttons.net/p/zero/1.0/88x31.png) ](http://creativecommons.org/publicdomain/zero/1.0/)
---
+++ url-shortener.js
@@ -0,0 +1,48 @@
+!function (shorten) {
+  chrome.browserAction.onClicked.addListener(function (tab) {
+    var url = tab.url || tab.pendingUrl;
+    var lastTimer, msg = function (msg, title) {
+      chrome.notifications.create('notify', {
+        type: 'basic',
+        title: title || chrome.i18n.getMessage('extName'),
+        iconUrl: 'icon.png',
+        message: msg
+      });
+      setTimeout(lastTimer = function t() {
+        if (lastTimer === t)
+          chrome.notifications.clear('notify');
+      }, 3000);
+    };
+    var APIList = chrome.runtime.getManifest().permissions.slice(3);
+    var Ajax = function (url, success, error) {
+      var xhr = new XMLHttpRequest();
+      xhr.open('GET', url);
+      xhr.onload = function () {
+        if (xhr.status === 200 && xhr.responseText && !xhr.responseText.indexOf('http'))
+          success(xhr.responseText);
+        else
+          error();
+      };
+      xhr.onerror = error;
+      xhr.send();
+    };
+    (shorten = function s() {
+      if (s !== shorten)
+        return;
+      if (!APIList.length) {
+        msg(chrome.i18n.getMessage('failed'));
+        return;
+      }
+      Ajax(APIList.shift() + encodeURIComponent(url), function (result) {
+        if (s !== shorten)
+          return;
+        var input = document.body.appendChild(document.createElement('input'));
+        input.value = result;
+        input.select();
+        document.execCommand('copy');
+        document.body.removeChild(input);
+        msg(tab.title || url, chrome.i18n.getMessage('copied'));
+      }, s);
+    })();
+  });
+}();
